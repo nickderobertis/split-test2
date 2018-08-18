5e46433fe25b3930a057b13c7744a4317eae4497
Nick DeRobertis <whoopnip@gmail.com>
2018-08-18 11:35:32 -0400

    Create module for analyzing outliers

---
+++ dero/data/typing.py
@@ -0,0 +1,12 @@
+from typing import Dict, Union, Tuple, Optional, List
+import pandas as pd
+
+
+SimpleDfDict = Dict[str, pd.DataFrame]
+DfOrDfDict = Union[SimpleDfDict, pd.DataFrame]
+DfDict = Dict[str, DfOrDfDict]
+DfDictOrNone = Union[DfDict, None]
+DfOrSeries = Union[pd.DataFrame, pd.Series]
+DfTuple = Tuple[DfOrSeries, DfOrSeries]
+DfTupleOrNone = Optional[DfTuple]
+FloatList = List[float]
