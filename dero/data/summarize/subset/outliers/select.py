96775541648366fc7c4324d15d15bbc921de7f6b
Nick DeRobertis <whoopnip@gmail.com>
2018-09-02 11:10:23 -0400

    Restructure data expand selections

--- dero/data/summarize/subset/outliers/select.py
+++ dero/data/summarize/subset/outliers/select.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from dateutil.relativedelta import relativedelta
 
+from dero.data.panel.expandselect import expand_entity_date_selections
 from dero.data.summarize.subset.outliers.typing import (
     StrList,
     AssociatedColDict,
@@ -115,31 +115,16 @@ def _col_bad_df_dict_to_selected_orig_df_dict(df: pd.DataFrame, col_bad_df_dict:
                                               expand_months: int = 3) -> DfDict:
     df_dict = {}
     for col, col_bad_df in col_bad_df_dict.items():
-        firm_date_df = _firm_date_range_df_from_df(
+        df_dict[col] = expand_entity_date_selections(
             col_bad_df,
+            [col] + always_associated_cols + associated_col_dict[col],
             num_firms=num_firms,
-            firm_id_col=firm_id_col,
-            date_col=date_col,
-            begin_datevar=begin_datevar,
-            end_datevar=end_datevar
-        )
-        _expand_date_df(
-            firm_date_df,
             expand_months=expand_months,
+            entity_id_col=firm_id_col,
+            date_col=date_col,
             begin_datevar=begin_datevar,
             end_datevar=end_datevar
         )
-        firm_df = _select_orig_df_from_date_df(
-            df,
-            firm_date_df,
-            col,
-            firm_id_col=firm_id_col,
-            date_col=date_col,
-            begin_datevar=begin_datevar,
-            end_datevar=end_datevar,
-            associated_cols=always_associated_cols + associated_col_dict[col]
-        )
-        df_dict[col] = firm_df
 
     return df_dict
 
@@ -152,45 +137,3 @@ def _sorted_bad_df_for_column(bad_df: pd.DataFrame, col: str, associated_cols: S
     ].sort_values(col, ascending=sort_ascending)
 
 
-def _firm_date_range_df_from_df(df: pd.DataFrame, num_firms: int = 3, firm_id_col: str = 'TICKER',
-                                date_col: str = 'Date',
-                                begin_datevar: str = 'Begin Date', end_datevar: str = 'End Date') -> pd.DataFrame:
-    firm_date_vars = [firm_id_col, date_col]
-
-    firms = df[firm_id_col].unique()[:num_firms]
-    firm_dates = df[df[firm_id_col].isin(firms)][firm_date_vars].sort_values(firm_date_vars)
-    earliest_dates = firm_dates.groupby(firm_id_col).min()
-    earliest_dates.rename(columns={date_col: begin_datevar}, inplace=True)
-    latest_dates = firm_dates.groupby(firm_id_col).max()
-    latest_dates.rename(columns={date_col: end_datevar}, inplace=True)
-    return earliest_dates.join(latest_dates)
-
-
-def _expand_date_df(date_df: pd.DataFrame, expand_months: int = 3,
-                    begin_datevar: str = 'Begin Date', end_datevar: str = 'End Date'):
-    """
-    Note: inplace
-    """
-    date_df[begin_datevar] = date_df[begin_datevar].apply(lambda x: x - relativedelta(months=expand_months))
-    date_df[end_datevar] = date_df[end_datevar].apply(lambda x: x + relativedelta(months=expand_months))
-
-
-def _select_orig_df_from_date_df(df: pd.DataFrame, date_df: pd.DataFrame, col: str,
-                                 firm_id_col: str = 'TICKER',
-                                 date_col: str = 'Date',
-                                 begin_datevar: str = 'Begin Date', end_datevar: str = 'End Date',
-                                 associated_cols: StrList = None) -> pd.DataFrame:
-    if associated_cols == None:
-        associated_cols = [firm_id_col, date_col]
-
-    out_df = pd.DataFrame()
-    for firm_id, date_series in date_df.iterrows():
-        selected_firm_df = df.loc[
-            (df[firm_id_col] == firm_id) &
-            (df[date_col] >= date_series[begin_datevar]) &
-            (df[date_col] <= date_series[end_datevar]),
-            associated_cols + [col]
-        ]
-        out_df = out_df.append(selected_firm_df)
-
-    return out_df
