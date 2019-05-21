61622cf222fc1601c4e531160ed279e73025908c
Nick DeRobertis <nick@claimfound.com>
2019-05-21 09:27:32 -0400

    Data: allow for negative windows in collect portfolios through time

--- dero/data/portfolio/resample.py
+++ dero/data/portfolio/resample.py
@@ -1,8 +1,10 @@
+from typing import Optional
 import pandas as pd
 import numpy as np
 
 
 def collect_portfolios_through_time(df: pd.DataFrame, portvar: str, id_var: str, needed_calendar_days: int,
+                                    needed_prior_calendar_days: Optional[int] = None,
                                     datevar: str = 'Date', portfolio_datevar: str = 'Portfolio Date'
                                     ) -> pd.DataFrame:
     output_df = pd.DataFrame()
@@ -13,8 +15,9 @@ def collect_portfolios_through_time(df: pd.DataFrame, portvar: str, id_var: str,
             [id_var, portvar]
         ].drop_duplicates()
         extended_date = port_date + np.timedelta64(needed_calendar_days, 'D')
+        begin_date = _portfolio_begin_date(port_date, needed_prior_calendar_days=needed_prior_calendar_days)
         port_df = df.loc[
-            (df[datevar] >= port_date) &
+            (df[datevar] >= begin_date) &
             (df[datevar] <= extended_date) &
             (df[id_var].isin(port_assignments[id_var]))
             ]
@@ -28,3 +31,9 @@ def collect_portfolios_through_time(df: pd.DataFrame, portvar: str, id_var: str,
         output_df = output_df.append(port_df)
 
     return output_df
+
+def _portfolio_begin_date(port_date: pd.Timestamp, needed_prior_calendar_days: Optional[int] = None) -> pd.Timestamp:
+    if needed_prior_calendar_days is None:
+        return port_date
+
+    return port_date - np.timedelta64(needed_prior_calendar_days, 'D')
